9d6213dc14a7a3bdc15c8c453dd715bf7bff581f
Dzaky Haidar <dzakyhdr@gmail.com>
2021-02-08 13:48:58 +0700

    LiveDataWithDataBinding

--- app/src/main/java/com/dzakyhdr/traindatabinding/MainActivity.kt
+++ app/src/main/java/com/dzakyhdr/traindatabinding/MainActivity.kt
@@ -8,26 +8,23 @@ import androidx.lifecycle.ViewModelProvider
 import com.dzakyhdr.traindatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var mainViewModelFactory: MainViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         mainViewModelFactory = MainViewModelFactory(0)
         viewModel = ViewModelProvider(this, mainViewModelFactory).get(MainViewModel::class.java)
-        viewModel.count.observe(this, {
-            binding.txtNumber.text = it.toString()
-        })
+        // for databinding wity livedata
+        binding.lifecycleOwner = this
+        // for viewmodel with databinding
+        binding.viewmodel = viewModel
 
-        binding.btnNumber.setOnClickListener {
-            viewModel.countNumber()
-        }
-
-        binding.btnKurang.setOnClickListener {
-            viewModel.decrement()
-        }
     }
 
 }
